d9e8c3fb4bce57920d499f788d685229c47fb635
Dan <ddupont2@uwo.ca>
2020-02-01 12:55:24 -0500

    basics of form done

--- lib/signIn.newUser.dart
+++ lib/signIn.newUser.dart
@@ -8,6 +8,10 @@ class newUser extends StatefulWidget {
 }
 
 class _newUserState extends State<newUser> {
+  // Guess this key is for data
+  final _formKey = GlobalKey<FormState>();
+  final double _fontSizeValue = 20.0;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,25 +30,228 @@ class _newUserState extends State<newUser> {
           ),
         ),
         child: Scaffold(
-          backgroundColor: Color(0x00000000),
-          appBar: AppBar(
-            title: Text('New Guest'),
-            flexibleSpace: Container(
-              decoration: new BoxDecoration(
-                gradient: new LinearGradient(
-                    colors: [
-                      const Color(0xFF00CCFF),
-                      const Color(0xFF3366FF),
-                    ],
-                    begin: const FractionalOffset(0.0, 0.0),
-                    end: const FractionalOffset(1.0, 0.0),
-                    stops: [0.0, 1.0],
-                    tileMode: TileMode.clamp),
+            backgroundColor: Color(0x00000000),
+            appBar: AppBar(
+              title: Text('New Guest'),
+              flexibleSpace: Container(
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        const Color(0xFF00CCFF),
+                        const Color(0xFF3366FF),
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.clamp),
+                ),
               ),
             ),
-          ),
-          body: Text("newUser Page"),
-        ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        // Add TextFormFields and RaisedButton here.
+//
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                            suffix: GestureDetector(
+                              onTap: () {
+                                print('tapped');
+                              },
+                              child: Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            labelText: 'First Name *',
+                            labelStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _fontSizeValue,
+                                fontFamily: 'AvenirLight'),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 0.5)),
+                          ),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: _fontSizeValue,
+                              fontFamily: 'AvenirLight'),
+                          //  controller: _passwordController,
+                        ),
+
+//
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                            suffix: GestureDetector(
+                              onTap: () {
+                                print('tapped');
+                              },
+                              child: Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            labelText: 'Last Name *',
+                            labelStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _fontSizeValue,
+                                fontFamily: 'AvenirLight'),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 0.5)),
+                          ),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: _fontSizeValue,
+                              fontFamily: 'AvenirLight'),
+                          //  controller: _passwordController,
+                        ),
+
+                        //
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                            suffix: GestureDetector(
+                              onTap: () {
+                                print('tapped');
+                              },
+                              child: Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            labelText: 'Organization',
+                            labelStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _fontSizeValue,
+                                fontFamily: 'AvenirLight'),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 0.5)),
+                          ),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: _fontSizeValue,
+                              fontFamily: 'AvenirLight'),
+                          //  controller: _passwordController,
+                        ),
+
+//
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                            suffix: GestureDetector(
+                              onTap: () {
+                                print('tapped');
+                              },
+                              child: Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            labelText: 'Email Address',
+                            labelStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _fontSizeValue,
+                                fontFamily: 'AvenirLight'),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 0.5)),
+                          ),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: _fontSizeValue,
+                              fontFamily: 'AvenirLight'),
+                          //  controller: _passwordController,
+                        ),
+
+//
+
+                        TextFormField(
+                          decoration: InputDecoration(
+                            suffix: GestureDetector(
+                              onTap: () {
+                                print('tapped');
+                              },
+                              child: Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            labelText: 'Purpose of Visit',
+                            labelStyle: TextStyle(
+                                color: Colors.white,
+                                fontSize: _fontSizeValue,
+                                fontFamily: 'AvenirLight'),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            enabledBorder: new UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white, width: 0.5)),
+                          ),
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: _fontSizeValue,
+                              fontFamily: 'AvenirLight'),
+                          //  controller: _passwordController,
+                        ),
+
+//
+
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: RaisedButton(
+                            onPressed: () {
+                              // Validate returns true if the form is valid, otherwise false.
+                              if (_formKey.currentState.validate()) {
+                                // If the form is valid, display a snackbar. In the real world,
+                                // you'd often call a server or save the information in a database.
+
+                                Scaffold.of(context).showSnackBar(
+                                    SnackBar(content: Text('Processing Data')));
+                              }
+                            },
+                            child: Text('Sign In'),
+                          ),
+                        ),
+
+//
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )),
       ),
     );
   }
